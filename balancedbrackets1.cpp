fdde641140ff82a6f7ce04cd99cdbb3b93dbc31c
divakararao76 <84025602+divakararao76@users.noreply.github.com>
2021-05-20 15:40:12 +0530

    Create balancedbrackets1.cpp

---
+++ balancedbrackets1.cpp
@@ -0,0 +1,55 @@
+// to check the given string is balanced brackets ot not
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        
+        string s;
+        cin>>s;
+        stack<char> st;
+        int f;
+        for(auto j:s)
+        {
+            switch(j)
+            {
+                case '{': st.push(j);break;
+                case '[': st.push(j);break;
+                case '(': st.push(j);break;
+                case '}': if(st.empty()|| st.top()!='{')
+                          f=0;
+                          st.pop();break;
+                case ']': if(st.empty()||st.top()!='[')
+                            f=0;
+                          st.pop(); break;
+                case ')': if(st.empty()||st.top()!='(')
+                          f=0;
+                          st.pop();break;
+            }
+        }
+        f=st.empty()?1:0;
+        if(f==1)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+        
+        
+        
+    }
+}
+
+/*
+output:
+2
+[{({]}})
+YES
+{[(}
+NO
+
+
+*/
